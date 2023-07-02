b0a389c2d7f9e9072b044689f95f3df16c272fd1
SeungYeon <yeondiary@gmail.com>
2023-07-02 17:07:30 +0900

    프로젝트 설정.

--- Game/Client/Client.cpp
+++ Game/Client/Client.cpp
@@ -1,8 +1,10 @@
 ﻿// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+#include "pch.h"
 #include "framework.h"
 #include "Client.h"
+#include "Game.h"
 
 #define MAX_LOADSTRING 100
 
@@ -42,18 +44,39 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CLIENT));
 
     MSG msg;
+    
+    // Game* game = new Game();
+    // 일반 포인터로 메모리 해제를 직접해주기 보다는 스마트 포인터 활용으로 변경.
+    unique_ptr<Game> game = make_unique<Game>();
+    game->Init();
 
     // 기본 메시지 루프입니다:
     // 해당 While문을 빠져나가기 전까지 내부 구문을 지속적으로 실행한다.
     // Message를 이용해서 키보드, 마우스의 입력을 받고 있다.
     // 실질적인 입력이 없으면 실행되지 않는다.
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (true) // GetMessage(&msg, nullptr, 0, 0)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        // PeekMessage 함수
+        // message 입력 (이벤트)
+        // handle 어떤 Windows를 사용하는지 지정, null 지정 시 현재 스레드와 관련된 모든 메시지
+        // filter min, max 특정 메시지를 필터하여 받을 지 여부, (0, 0)일 때 모든 메시지
+        // option 메세지 큐에서 살펴보고 큐에서 꺼낼 지 여부
+   
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            // 프로그램 종료
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
         }
+
+        // TODO: 게임 로직 추가.
+        // game->Update();
     }
 
     return (int) msg.wParam;
@@ -80,7 +103,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CLIENT));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
     wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_CLIENT);
+    wcex.lpszMenuName   = nullptr; // 파일, 도움말 메뉴 제거. MAKEINTRESOURCEW(IDC_CLIENT);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
---
+++ Game/Client/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// pragma: 전처리 단계
+#pragma comment(lib, "Engine.lib")
+
+#include "EnginePch.h"
